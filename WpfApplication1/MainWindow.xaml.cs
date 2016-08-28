0571b2f5322fc21e7f6c437edfe1010927488cf4
fishball <fishball@FISH>
2016-08-28 22:46:32 +0800

    1

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -5,7 +5,8 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
-
+using System.IO;
+using System.Windows.Controls;
 namespace WpfApplication1
 {
 
@@ -30,6 +31,40 @@ namespace WpfApplication1
                         lvDataBinding.ItemsSource = items;
                         lvDataBinding2.ItemsSource = items;
             lvUsers.ItemsSource = items;
+
+            List<Person> persons = new List<Person>();
+            Person person1 = new Person() { Name = "John Doe", Age = 42 };
+
+            Person person2 = new Person() { Name = "Jane Doe", Age = 39 };
+
+            Person child1 = new Person() { Name = "Sammy Doe", Age = 13 };
+            person1.Children.Add(child1);
+            person2.Children.Add(child1);
+
+            person2.Children.Add(new Person() { Name = "Jenny Moe", Age = 17 });
+
+            Person person3 = new Person() { Name = "Becky Toe", Age = 25 };
+
+            persons.Add(person1);
+            persons.Add(person2);
+            persons.Add(person3);
+
+            person2.IsExpanded = true;
+            person2.IsSelected = true;
+
+            trvPersons.ItemsSource = persons;
+
+            //drei
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drives)
+                trvStructure.Items.Add(CreateTreeItem(driveInfo));
+            //vier
+            List<User> users3 = new List<User>();
+            users3.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users3.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users3.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users3;
         }
 
         private void btnAddUser_Click(object sender, RoutedEventArgs e)
@@ -83,8 +118,67 @@ namespace WpfApplication1
         {
 
         }
+        
+        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (trvPersons.SelectedItem != null)
+            {
+                var list = (trvPersons.ItemsSource as List<Person>);
+                int curIndex = list.IndexOf(trvPersons.SelectedItem as Person);
+                if (curIndex >= 0)
+                    curIndex++;
+                if (curIndex >= list.Count)
+                    curIndex = 0;
+                if (curIndex >= 0)
+                    list[curIndex].IsSelected = true;
+            }
+        }
+        //drei
+        private void btnToggleExpansion_Click(object sender, RoutedEventArgs e)
+        {
+            if (trvPersons.SelectedItem != null)
+                (trvPersons.SelectedItem as Person).IsExpanded = !(trvPersons.SelectedItem as Person).IsExpanded;
+        }
+        public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
 
+                DirectoryInfo expandedDir = null;
+                if (item.Tag is DriveInfo)
+                    expandedDir = (item.Tag as DriveInfo).RootDirectory;
+                if (item.Tag is DirectoryInfo)
+                    expandedDir = (item.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                        item.Items.Add(CreateTreeItem(subDir));
+                }
+                catch { }
+            }
+        }
+
+        private TreeViewItem CreateTreeItem(object o)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.Header = o.ToString();
+            item.Tag = o;
+            item.Items.Add("Loading...");
+            return item;
+        }
+        //vier
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime Birthday { get; set; }
+        }
     }
+
 
     public class User2
     {
@@ -122,5 +216,57 @@ namespace WpfApplication1
         }
     }
 
-   
+    public class Person : TreeViewItemBase
+    {
+        public Person()
+        {
+            this.Children = new ObservableCollection<Person>();
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public ObservableCollection<Person> Children { get; set; }
+    }
+
+    public class TreeViewItemBase : INotifyPropertyChanged
+    {
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return this.isSelected; }
+            set
+            {
+                if (value != this.isSelected)
+                {
+                    this.isSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        private bool isExpanded;
+        public bool IsExpanded
+        {
+            get { return this.isExpanded; }
+            set
+            {
+                if (value != this.isExpanded)
+                {
+                    this.isExpanded = value;
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
 }
